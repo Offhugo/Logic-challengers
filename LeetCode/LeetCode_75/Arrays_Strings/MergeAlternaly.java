56c55420799ced03aea0b0a03e3fa14c1bae4d33
Hugo Mendes <hugo.mendes.313@gmail.com>
2026-07-16 22:15:15 -0300

    Comentando atividade

--- LeetCode/LeetCode_75/Arrays_Strings/MergeAlternaly.java
+++ LeetCode/LeetCode_75/Arrays_Strings/MergeAlternaly.java
@@ -7,22 +7,32 @@ public class MergeAlternaly {
     * */
 
     public String mergeAlternately(String word1, String word2) {
+        // String builder para construir a nova string
         StringBuilder sb = new StringBuilder();
+
+        // Ponteiro incrementer cada letra
         int i = 0;
 
+        // Loop para construir a lógica
+        // As condicionais aplicam a regra para cada word
+        // se uma delas acabar, tudo bem, a regra do while garante a continuidade, para ambos
         while (i < word1.length() || i < word2.length()) {
 
+            // Primeira condicional
             if (i < word1.length()) {
                 sb.append(word1.charAt(i));
             }
 
+            // Segunda condicional
             if (i < word2.length()) {
                 sb.append(word2.charAt(i));
             }
 
+            // Ponteiro avançando ao final do ciclo
             i++;
         }
 
+        // String definitiva
         return sb.toString();
     }
 }
