5ab5be758befd3c9af1b8b1e170862d89a639cf4
Hugo Mendes <hugo.mendes.313@gmail.com>
2026-09-10 19:35:47 -0300

    feat: Fiz o classico algoritmo TwoSum, estou muito feliz por iss!

---
+++ LeetCode/LeetCode_75/HashMap/TwoSum.java
@@ -0,0 +1,22 @@
+package Logic_Problems.LeetCode.LeetCode_75.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+        public int[] resolucaoTwoSum(int target, int[] nums) {
+            Map<Integer, Integer> hashMap = new HashMap<>();
+            int complemento = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                complemento = target - nums[i];
+
+                if (hashMap.containsKey(complemento)) {
+                    return new int[]{ hashMap.get(complemento), i };
+                };
+
+                hashMap.put(nums[i], i);
+            }
+            return null;
+        }
+}
